7a4d92fcbb8c1f2c82ea511815ad6c9eb9ef5f01
Alexander Novitchkov-Burbank <seraphim7@outlook.com>
2023-10-07 10:28:20 -0700

    Hw1 (#2)
    * divInDivSelector renamed

--- hw1/01-selectors.md
+++ hw1/01-selectors.md
@@ -2,6 +2,33 @@
 
 1. plate
 2. bento
-3.
-4.
-5. ...
+3. #fancy
+4. plate > apple
+5. #fancy > pickle
+6. .small
+7. orange.small
+8. bento > orange.small
+9. plate, bento
+10. *
+11. plate > *
+12. plate + apple
+13. bento ~ pickle
+14. plate > apple
+15. plate > orange:first-child
+16. plate > apple, plate > pickle
+17. plate > apple, pickle
+18. nth-child(3)
+19. bento:nth-of-type(1)
+20. apple:first
+21. :nth-of-type(2), :nth-of-type(4), :nth-of-type(6)
+22. :nth-of-type(3), :nth-of-type(5)
+23. :nth-child(2) > apple
+24. :nth-child(2), :nth-of-child(6)
+25. :first, :last
+26. apple:not(.small)
+27. [for]
+28. [for="Sarah"], [for="Luke"]
+29. [for="Vitaly"]
+30. [for="Sam"], [for="Sarah"]
+31. [for$="o"]
+32. [for*="obb"]
--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,54 @@
+// I used the bootstrap guide https://getbootstrap.com/docs/5.0/getting-started/introduction/
+// I looked inside the imported css bootstrap file https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css
+// I looked at the Airbnb style guide https://github.com/airbnb/javascript#airbnb-javascript-style-guide-
+
+const htmlBodySelector = document.querySelector('body');
+const h1Selector = document.querySelector('h1');
+const sectionSelector = document.querySelector('section');
+const paragraphSelector = document.querySelector('p');
 const elem = document.querySelector('input');
+
+htmlBodySelector.className = 'bg-primary d-flex justify-content-center';
+sectionSelector.className = 'bg-light mt-5 pt-3 pb-3 ps-3 pe-4 rounded';
+h1Selector.className = 'fs-2 mb-3';
+paragraphSelector.className = 'mb-2 mt-4';
+elem.className = 'h-75';
+
+const handleInput = function handleInputFromUI() {
+  const responseIfPalindrome = document.querySelector('div > div');
+
+  let invalidInput = false;
+  let palindrome = true;
+
+  for (let i = 0; i < Math.floor(elem.value.length / 2); i++) {
+    if (
+      elem.value[i] >= 0 &&
+      elem.value[i] <= 9 &&
+      elem.value[elem.value.length - 1 - i] >= 0 &&
+      elem.value[elem.value.length - 1 - i] <= 9
+    ) {
+      if (elem.value[i] !== elem.value[elem.value.length - 1 - i]) {
+        palindrome = false;
+      }
+    } else {
+      invalidInput = true;
+    }
+  }
+
+  if (elem.value.length === 0 || invalidInput) {
+    responseIfPalindrome.className = 'fail';
+    responseIfPalindrome.innerHTML = 'Invalid input!';
+  } else {
+    if (palindrome) {
+      responseIfPalindrome.className = 'success';
+      responseIfPalindrome.innerHTML = 'This is a palindrome!';
+    } else {
+      responseIfPalindrome.className = 'fail';
+      responseIfPalindrome.innerHTML = 'No. Try again.';
+    }
+  }
+
+  responseIfPalindrome.classList.add('mt-3');
+};
 
 elem.addEventListener('input', handleInput);
