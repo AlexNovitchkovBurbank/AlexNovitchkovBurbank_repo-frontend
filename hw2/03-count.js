1f07ef1d39105563429928dfb776b0fa4e74a6d7
AlexNovitchkovBurbank <seraphim7@outlook.com>
2023-10-07 21:48:16 -0700

    1 and 2 are complete. Structure for 3 is built

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,9 @@
-// Add your code here
+const input = document.querySelector('input');
+
+const handleKeyDown = function findInParagraph(wordToFind) {
+  const text = document.querySelector('div:last-child');
+
+  console.log(text.innerText);
+};
 
 input.addEventListener('keydown', handleKeyDown);
