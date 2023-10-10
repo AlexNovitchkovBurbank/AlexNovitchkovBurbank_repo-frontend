b41e98213596b7445403b90e17b3c1d90ec7c183
AlexNovitchkovBurbank <seraphim7@outlook.com>
2023-10-10 12:20:46 -0700

    Changes in 03 and 04

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,9 +1,61 @@
 const input = document.querySelector('input');
+const textSelector = document.querySelector('div:last-child');
 
-const handleKeyDown = function findInParagraph(wordToFind) {
-  const text = document.querySelector('div:last-child');
+const stringToArray = function BreakTextInArrayOfWords(text) {
+  let formattedText = text.replaceAll('       ', '').trim();
 
-  console.log(text.innerText);
+  const arrayOfText = formattedText.split(' ');
+
+  return arrayOfText;
+};
+
+const resetText = function setTextNoHighlights(text) {
+  const originalWordsArray = stringToArray(text);
+
+  const wordsWithoutHighlights = originalWordsArray.map((word) => {
+    if (word.startsWith('class=')) {
+      return word
+        .replaceAll('class="bg-warning">', '')
+        .replaceAll('</span>', '');
+    } else if (word.startsWith('<span')) {
+      return '';
+    }
+
+    return word;
+  });
+
+  let originalText = wordsWithoutHighlights.join(' ');
+
+  console.log(originalText);
+
+  textSelector.innerHTML = originalText;
+};
+
+const highlightWords = function highlightText(text) {
+  const modifiedWordsArray = stringToArray(text);
+
+  const wordToFind = input.value;
+
+  if (wordToFind.length === 0) return false;
+
+  const wordsWithHighlights = modifiedWordsArray.map((word) => {
+    if (word === wordToFind) {
+      return `<span class="bg-warning">${word}</span>`;
+    } else if (word === wordToFind + ',' || word === wordToFind + '.') {
+      return `<span class="bg-warning">${word}</span>`;
+    }
+
+    return word;
+  });
+
+  let originalText = wordsWithHighlights.join(' ');
+
+  textSelector.innerHTML = originalText;
+};
+
+const handleKeyDown = function findInParagraph(eventContainingWord) {
+  resetText(textSelector.innerHTML);
+  highlightWords(textSelector.innerHTML);
 };
 
 input.addEventListener('keydown', handleKeyDown);
--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,17 @@
-// Add your code here
+const input = document.querySelector('input');
+
+const changeColor = function changePageBackgroundColor() {
+  const red = ((Math.random() + 1) * 100) % 255;
+  const green = ((Math.random() + 1) * 100) % 255;
+  const blue = ((Math.random() + 1) * 100) % 255;
+
+  document.body.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 75%)`;
+};
+
+input.addEventListener('input', () => {
+  const waitTimeInSeconds = input.value * 1000;
+
+  console.log(waitTimeInSeconds);
+
+  setInterval(changeColor, waitTimeInSeconds);
+});
