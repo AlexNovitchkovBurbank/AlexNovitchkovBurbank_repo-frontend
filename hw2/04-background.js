5ca0e8759cce0437a6183fef28bd0b764d46dda9
ryzen <ryzen@DESKTOP-NNHA2AR>
2023-10-12 19:11:40 -0700

    trying to find solution to 04

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,24 +1,33 @@
 window.addEventListener('load', () => {
-  const submitEvent = document.querySelector('button');
+  let intervalId = setInterval(changeColor, 3 * 1000);
+  const stopEvent = document.querySelector('#stop');
 
-  submitEvent.addEventListener('click', () => {
-    const input = document.querySelector('[type=number]');
-    if (input.value.length === 0) {
-      resetInterval();
-    } else {
-      setInterval(changeColor, input.value * 1000);
-    }
+  console.log(intervalId);
+  
+  const input = document.querySelector('[type=number]');
+
+  if (input.textContent.length > 0) {
+    console.log("input.textContent: " + input.textContent)
+    clearInterval(intervalId);
+    intervalId = setInterval(changeColor, input.textContent * 1000);
+  } else {
+    resetInterval(intervalId);
+  }
+  
+  stopEvent.addEventListener('click', () => {
+    resetInterval(intervalId);
   });
 });
 
 const changeColor = function changePageBackgroundColor() {
-  const red = Math.floor((Math.random() + 1) * 100) % 255;
-  const green = Math.floor((Math.random() + 1) * 100) % 255;
-  const blue = Math.floor((Math.random() + 1) * 100) % 255;
+  const red = Math.floor(Math.random() * 100) % 255;
+  const green = Math.floor(Math.random() * 100) % 255;
+  const blue = Math.floor(Math.random() * 100) % 255;
 
-  document.body.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 75%)`;
+  document.body.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 50%)`;
 };
 
-const resetInterval = function resetBackgroundColorInterval() {
+const resetInterval = function resetBackgroundColorInterval(intervalId) {
+  clearInterval(intervalId);
   setInterval(changeColor, 3 * 1000);
 };
