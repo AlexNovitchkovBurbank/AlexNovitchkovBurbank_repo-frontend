ff3b9823a299a42e736e828cbe3c58095a89c6e0
AlexNovitchkovBurbank <seraphim7@outlook.com>
2023-10-24 12:20:12 -0700

    More changes

--- hw3/react/chartapp/src/search.js
+++ hw3/react/chartapp/src/search.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './search.css';
+import { Axios } from 'axios';
 
 const url = '';
+
+const getThronesApi = new Axios();
+
+getThronesApi.get(url, {}).then((apiData) => console.log(apiData.data));
+
+document.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const resultBlock = document.querySelector('#results');
+});
 
 const SearchComponent = function searchCharacters() {
   return (
     <div>
+      <h1>Search</h1>
       <form>
-        <input type='text' className='bg-dark'></input>
+        <label htmlFor='searchBox' className='me-2'>
+          Search for character:
+        </label>
+        <input type='text' className='form-input' id='searchBox' />
+        <button type='submit' className='btn-primary'>
+          submit
+        </button>
       </form>
+      <div id='results'></div>
     </div>
   );
 };
